7032a7a9ad0be794027ab887ef767985727e7c82
SANDEEP9496 <sandeepmk1996@outlook.com>
2024-07-22 17:34:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"  # Must be globally unique
+  name                     = "examplestoracc1208"  # Must be globally unique
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
