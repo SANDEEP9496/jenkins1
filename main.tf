25fd01222116b50a6f4e7375c67db3194308061a
SANDEEP9496 <sandeepmk1996@outlook.com>
2024-07-22 17:32:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "resourcegroupname" "example" {
+resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "East US"
+  location = "West Europe"  # Ensure location is enclosed in quotes
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplestoracc"  # Must be globally unique
-  resource_group_name      = resourcegroupname
-  location                 = East US
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
