41959081c70a6e9914424b621f1f9aeee1f3f6ce
SANDEEP9496 <sandeepmk1996@outlook.com>
2024-07-22 18:09:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,34 +14,3 @@ resource "azurerm_storage_account" "example" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-
-resource "azurerm_role_definition" "example" {
-    name        = "example_role"
-    scope       = data.azurerm_subscription.primary.id
-    description = "Example role"
-    
-    permissions {
-        actions   = ["*"]
-        not_actions = []
-    }
-}
-
-resource "azurerm_policy_definition" "example" {
-    name         = "example_policy"
-    policy_type   = "Custom"
-    mode          = "All"
-    display_name  = "Example Policy"
-    description   = "Example policy description"
-
-    policy_rule = <<POLICY
-    {
-        "if": {
-            "field": "location",
-            "equals": "eastus"
-        },
-        "then": {
-            "effect": "deny"
-        }
-    }
-    POLICY
-}
